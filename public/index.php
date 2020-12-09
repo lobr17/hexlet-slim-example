2638dceb2e789b82a80870ad1e5c106dfb1e0437
Konstantin <konpichugin@yandex.ru>
2020-12-09 13:59:43 +0300

    work version without router, update, delete

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@ use Slim\Middleware\MethodOverrideMiddleware;
 use DI\Container;
 
 
-require '/composer/vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 $container = new Container();
 $container->set('renderer', function () {
@@ -18,16 +18,37 @@ $app->add(MethodOverrideMiddleware::class);
 $app->addErrorMiddleware(true, true, true);
 
 $app->get('/', function ($request, $response) {
+    return $this->get('renderer')->render($response, 'index.phtml');
+});
+
+$app->get('/users', function ($request, $response) {	
     $users = json_decode($request->getCookieParam('users', json_encode([])), true);
     $params = [
         'users' => $users
     ];
-    return $this->get('renderer')->render($response, 'index.phtml', $params);
+
+    return $this->get('renderer')->render($response, "users/index.phtml", $params);
 });
 
-// BEGIN (write your solution here)
+$app->get('/users/new', function ($request, $response) {
+    	
+    $params = [
+        'user' => [],
+        //'errors' => []
+    ];
+    return $this->get('renderer')->render($response, 'users/new.phtml', $params);
+});
 
-// END
+$app->post('/users', function ($request, $response) {
+    $user = $request->getParsedBodyParam('user');
+    $users = json_decode($request->getCookieParam('users', json_encode([])));
+    $users[] = $user;
+    $encodedUsers = json_encode($users);
+
+    return $response->withHeader('Set-Cookie', "users={$encodedUsers};Path=/users")
+        ->withRedirect('/users');
+});
+
 
 $app->run();
 
